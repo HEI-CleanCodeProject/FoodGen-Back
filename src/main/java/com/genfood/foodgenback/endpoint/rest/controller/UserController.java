775229342d6ac87654b888df0916913836327901
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2024-01-24 21:23:20 +0300

    chore: add user service

--- src/main/java/com/genfood/foodgenback/endpoint/rest/controller/UserController.java
+++ src/main/java/com/genfood/foodgenback/endpoint/rest/controller/UserController.java
@@ -1,29 +1,30 @@
 package com.genfood.foodgenback.endpoint.rest.controller;
 
-import com.genfood.foodgenback.endpoint.rest.controller.validator.MailValidator;
+import com.genfood.foodgenback.endpoint.rest.controller.entity.UserEntity;
+import com.genfood.foodgenback.endpoint.rest.controller.mapper.UserMapper;
 import com.genfood.foodgenback.model.User;
 import com.genfood.foodgenback.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class UserController {
-  private final UserService userService;
-  private final MailValidator mailValidator;
+    private final UserService userService;
+    private final UserMapper userMapper;
 
-  @PutMapping("/user")
-  public User Save(@RequestBody User userEntity) {
-    mailValidator.accept(userEntity);
-    return userService.saveUser(userEntity);
-  }
 
-  @GetMapping("/user/{username}")
-  public User getByUsername(@PathVariable String username) {
-    return userService.getUserByUsername(username);
-  }
+    @PutMapping("/user")
+    public User Save(@RequestBody UserEntity userEntity){
+        return userService.saveUser(userMapper.ToRestUser(userEntity));
+    }
+
+    @GetMapping("/user/{username}")
+    public User getUserByUserName(@RequestParam String name){
+        return userService.getUserByName(name);
+    }
 }
