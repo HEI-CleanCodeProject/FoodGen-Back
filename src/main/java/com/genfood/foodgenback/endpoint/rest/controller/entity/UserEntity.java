2c8d96a78482f50c80ebe825fbf6327bf6049aea
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2024-01-24 21:48:07 +0300

    chore: add unique constraint to mail

--- src/main/java/com/genfood/foodgenback/endpoint/rest/controller/entity/UserEntity.java
+++ src/main/java/com/genfood/foodgenback/endpoint/rest/controller/entity/UserEntity.java
@@ -14,6 +14,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserEntity {
+    private Long id;
     private String username;
     private String firstname;
     private String lastname;
