569cba1916268e738d3232cd3c2b47998586ea0d
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2024-01-24 21:48:07 +0300

    chore: add unique constraint to mail

--- src/main/java/com/genfood/foodgenback/model/User.java
+++ src/main/java/com/genfood/foodgenback/model/User.java
@@ -1,11 +1,13 @@
 package com.genfood.foodgenback.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,6 +30,8 @@ public class User {
     private String username;
     private String firstname;
     private String lastname;
+
+    @Column(unique=true)
     private String mail;
     private String password;
 }
