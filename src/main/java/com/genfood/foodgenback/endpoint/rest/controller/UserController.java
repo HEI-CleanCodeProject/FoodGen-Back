f5788a9f03f47b08a8d9e043837efd1f57b5b763
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2024-01-24 21:23:20 +0300

    chore: add user service

--- src/main/java/com/genfood/foodgenback/endpoint/rest/controller/UserController.java
+++ src/main/java/com/genfood/foodgenback/endpoint/rest/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.genfood.foodgenback.endpoint.rest.controller;
 
+import com.genfood.foodgenback.endpoint.rest.controller.entity.UserEntity;
+import com.genfood.foodgenback.endpoint.rest.controller.mapper.UserMapper;
+import com.genfood.foodgenback.model.User;
+import com.genfood.foodgenback.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
 public class UserController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+
+    @PutMapping("/user")
+    public User Save(@RequestBody UserEntity userEntity){
+        return userService.saveUser(userMapper.ToRestUser(userEntity));
+    }
+
+    @GetMapping("/user/{username}")
+    public User getUserByUserName(@RequestParam String name){
+        return userService.getUserByName(name);
+    }
 }
--- src/main/java/com/genfood/foodgenback/repository/UserRepository.java
+++ src/main/java/com/genfood/foodgenback/repository/UserRepository.java
@@ -1,17 +1,15 @@
 package com.genfood.foodgenback.repository;
 
 import com.genfood.foodgenback.model.User;
-import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @EnableJpaRepositories
 @Repository
-public interface UserRepository extends JpaRepository<User,String> {
+public interface UserRepository extends JpaRepository<User,Long> {
     User findByUsername(String name);
 
-    User getById(String id);
-
-
+    User getById(Long id);
+    User findByMail(String mail);
 }
