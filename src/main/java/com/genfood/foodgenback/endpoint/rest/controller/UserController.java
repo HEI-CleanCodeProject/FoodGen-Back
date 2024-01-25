55a193bcab79c1cb1873bf088e951ce266d4eb79
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2024-01-25 12:35:44 +0300

    fix:make username to path variable

--- src/main/java/com/genfood/foodgenback/endpoint/rest/controller/UserController.java
+++ src/main/java/com/genfood/foodgenback/endpoint/rest/controller/UserController.java
@@ -5,6 +5,7 @@ import com.genfood.foodgenback.model.User;
 import com.genfood.foodgenback.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +24,7 @@ public class UserController {
     }
 
     @GetMapping("/user/{username}")
-    public User getUserByUserName(@RequestParam String name){
-        return userService.getUserByName(name);
+    public User getByUserName(@PathVariable String username){
+        return userService.getUserByName(username);
     }
 }
