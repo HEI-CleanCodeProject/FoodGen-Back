1bbd817037f510c780f5f8eb71e92631d17b4bd8
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2024-01-25 09:19:22 +0300

    feat: add health controller

---
+++ src/main/java/com/genfood/foodgenback/endpoint/rest/controller/PingController.java
@@ -0,0 +1,17 @@
+package com.genfood.foodgenback.endpoint.rest.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class PingController {
+
+
+    @GetMapping("/ping")
+    public String HealthController(){
+        return "Hello world";
+    }
+}
